69c77f808d29a778ab58decdbfd1b7a5db23b18c
Saleh Arif <saleharif209@gmail.com>
2023-06-14 14:05:12 +0500

    init

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(){
+  
+  return (
+    <View>
+        <Text> abcd</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  }
+});
+
+export default App;
